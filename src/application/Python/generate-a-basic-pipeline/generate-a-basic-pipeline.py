1323042549638d21bbbafe7247a58ef1d27979f0
paul.lin <paul.lin@adlinktech.com>
2022-01-05 17:33:46 +0800

    modify python basic pipeline

--- src/application/Python/generate-a-basic-pipeline/generate-a-basic-pipeline.py
+++ src/application/Python/generate-a-basic-pipeline/generate-a-basic-pipeline.py
@@ -11,38 +11,11 @@ gi.require_version('Gst', '1.0')
 from gi.repository import Gst, GObject, GLib
 
 
-pipelineOutputQueue = Queue()
-
-
-def extract_data(sample):
-    buf = sample.get_buffer()
-    caps = sample.get_caps()
-    
-    # ref: get some basic information
-    # print(caps.get_structure(0).get_value('format'))
-    # print(caps.get_structure(0).get_value('height'))
-    # print(caps.get_structure(0).get_value('width'))
-    # print(buf.get_size())
-
-    arr = numpy.ndarray(
-        (caps.get_structure(0).get_value('height'),
-         caps.get_structure(0).get_value('width'),
-         3),
-        buffer=buf.extract_dup(0, buf.get_size()),
-        dtype=numpy.uint8)
-    return arr
-
-
-def new_sample(sink, data) -> Gst.FlowReturn:
-    sample = sink.emit('pull-sample')
-    arr = extract_data(sample)
-    pipelineOutputQueue.put(arr.copy())
-    return Gst.FlowReturn.OK
-
-
 def on_message(bus: Gst.Bus, message: Gst.Message, loop: GObject.MainLoop):
     mtype = message.type
 
+    print('in on_message')
+    
     if mtype == Gst.MessageType.EOS:
         print("End of stream")
         loop.quit()
@@ -51,7 +24,7 @@ def on_message(bus: Gst.Bus, message: Gst.Message, loop: GObject.MainLoop):
         err, debug = message.parse_error()
         print(err, debug)
         loop.quit()
-    elif mtype == Gst.MessageType.WARNING:
+    elif mtype == Gst.MessageType.ANY:
         err, debug = message.parse_warning()
         print(err, debug)
 
@@ -100,6 +73,7 @@ if __name__ == '__main__':
         print("Start to run the pipeline.\n")
         loop.run()
     except Exception:
+        print("in exception")
         traceback.print_exc()
         loop.quit()
 
